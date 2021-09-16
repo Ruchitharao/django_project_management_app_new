930137a656d6619aa3fe4c4805ba66d0bd2832d9
Ruchitharao <raoruchi.preetham@gmail.com>
2021-09-16 13:37:05 -0700

    updated settings

--- settings.py
+++ settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["mydjangoruchiapp.herokuapp.com"]
+ALLOWED_HOSTS = ['mydjangoruchiapp.herokuapp.com']
 
 
 # Application definition
